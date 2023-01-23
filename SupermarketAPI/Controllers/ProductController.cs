b54d26f8e6efc0c39ac901e0b3ae180aab26469e
Igor Carlos <igor.lima58@gmail.com>
2023-01-23 02:10:09 -0300

    first commit

---
+++ SupermarketAPI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupermarketAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>();
+        private static int id = 0;
+
+        [HttpPost]
+        public IActionResult AddProduct([FromBody]Product product)
+        {
+            product.Id = id++;
+            products.Add(product);
+            Console.WriteLine(product.Name);
+            return CreatedAtAction(
+                nameof(GetProductById), new { Id = product.Id }, product);
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts() 
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product product = products.FirstOrDefault(product => product.Id == id);
+            if(product != null)
+            {
+                return Ok(product);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ SupermarketAPI/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "product name is required")]
+        [StringLength(30, ErrorMessage = "must be less than 30 characters")]
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public double Category { get; set; }
+        [Required(ErrorMessage = "price is required")]
+        public double Price { get; set; }
+        public double Weight { get; set; }
+    }
+}
